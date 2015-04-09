8e2c61604b3d9eef888cda89a9a8df5229c99d79
Ubuntu <ubuntu@ip-172-31-20-225.us-west-2.compute.internal>
2015-04-09 01:08:11 +0000

    Initial Commit - hello world

---
+++ HelloWorld.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+import           Yesod
+
+data App = App
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+myLayout :: Widget -> Handler Html
+myLayout widget = do
+    pc <- widgetToPageContent $ do
+        widget
+        toWidget [lucius| body { font-family: verdana } |]
+    withUrlRenderer
+        [hamlet|
+            $doctype 5
+            <html>
+                <head>
+                    <title>#{pageTitle pc}
+                    <meta charset=utf-8>
+                    ^{pageHead pc}
+                <body>
+                    <article>
+                        ^{pageBody pc}
+        |]
+
+instance Yesod App where
+    defaultLayout = myLayout
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout
+    [whamlet|
+        <p>Hello World!
+    |]
+
+main :: IO ()
+main = warp 3000 App
